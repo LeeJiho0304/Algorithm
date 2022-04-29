fcac3b418ed2d46b287b7e63a422af91e4e05eaf
LeeJiho0304 <leejiho0304@gmail.com>
2022-04-29 19:51:11 +0900

    1157번 완료

---
+++ 1157.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1157
+# 백준 1157번
+
+word = input().upper()
+word_list = list(set(word))
+count_list = []
+
+for i in word_list:
+    cnt = word.count(i)
+    count_list.append(cnt)
+
+if count_list.count(max(count_list)) > 1:
+    print('?')
+else:
+    max_index = count_list.index(max(count_list))
+    print(word_list[max_index])
