e3ce522bd3d42629dc9887c1b363fc3678567868
LeeJiho0304 <leejiho0304@gmail.com>
2022-08-29 20:30:38 +0900

    백준11729

--- Java/backjoon/BOJ11720.java
+++ Java/backjoon/BOJ11720.java
@@ -1,22 +1,22 @@
-package java.backjoon;
+package Java.backjoon;
 
 import java.util.Scanner;
 public class BOJ11720 {
-    // https://www.acmicpc.net/problem/11720
-    // 백준 11720번 숫자의 합
-
     public static void main(String[] args) {
+    
+        // https://www.acmicpc.net/problem/11720
+        // 백준 11720번 숫자의 합
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        // 입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변한하기
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
-
+        
+        int N = Integer.parseInt(sc.nextLine());
+        String[] strNum = sc.nextLine().split("");
         int sum = 0;
-        for (int i = 0; i < cNum.length; i++) {
-            sum += cNum[i] - '0'; // cNum[i]를 정수형을 변환하면서 sum에 더하여 누적하기
+
+        for (int i = 0; i < N; i++) {		
+            sum += Integer.parseInt(strNum[i]);
         }
 
         System.out.println(sum);
     }
 }
+
