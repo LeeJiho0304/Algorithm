c1a3f9d09ef9d92de72cada6fb971c0bae09f129
Lee Jiho <102660877+LeeJiho0304@users.noreply.github.com>
2022-09-28 19:15:14 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+
+        for(int i=0;i<commands.length;i++){
+
+            int beginIndex = (commands[i][0])-1;
+            int endIndex = commands[i][1];
+            int k = (commands[i][2])-1;
+
+            //시작index, 끝index 기준으로 자른 배열 복사하기
+            int temp[] = Arrays.copyOfRange(array, beginIndex, endIndex);
+
+            //복사된 배열 오름차순으로 정렬
+            Arrays.sort(temp);
+
+            //k번째 배열 answer 배열에 담기
+            answer[i] = temp[k];
+        }
+        return answer;
+    }
+}
